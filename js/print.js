8cebc89e6ee06beac5590fc9d5f218fe54ad640d
Danielle Hill <goal2code@gmail.com>
2015-08-07 20:43:46 -0400

    Added two new methods

--- js/print.js
+++ js/print.js
@@ -1,11 +1,23 @@
 var Print = (function() {
 	
 	var userInfo = function(name, points, categories) {
-		var msg = name + " has earned " + points + " total points in " + categories + " different categories!";
-		return msg;
+		var a = name + " has earned " + points + " total points in " + categories + " different categories!";
+		return a;
+	};
+
+	var footer = function(path) {
+		var b = "Data source: <a href=" + path + " target=\"_blank\">Treehouse profile JSON</a>";
+		return b;
+	};
+
+	var jsonFail = function(user, sample1, sample2) {
+		var c = "Sorry, no profiles match " + user + ". Try <a href=\"#" + sample1 + "\"> " +sample1 + "</a> or <a href=\"#" + sample2 + "\">" +sample2 +"</a>";
+		return c;
 	};
 	
 	return {
-		userInfo: userInfo
+		userInfo: userInfo,
+		footer: footer,
+		jsonFail: jsonFail
 	};	
 })();
