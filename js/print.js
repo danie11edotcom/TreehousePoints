8dba9064cb4652d8eb89ea432912fea77c845761
Danielle Hill <goal2code@gmail.com>
2015-08-07 20:30:16 -0400

    Updated userInfo module

--- js/print.js
+++ js/print.js
@@ -2,6 +2,7 @@ var Print = (function() {
 	
 	var userInfo = function(name, points, categories) {
 		var msg = name + " has earned " + points + " total points in " + categories + " different categories!";
+		return msg;
 	};
 	
 	return {
