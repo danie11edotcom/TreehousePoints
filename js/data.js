b8a9bba3a25683671b0b4dcc7c3c4da8a7bc2ee1
Danielle Hill <goal2code@gmail.com>
2015-08-15 17:35:29 -0400

    Added modules for data manipulation of badge data to Data module

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ var Data = (function() {
     for (var val in arr) {  //for ES6 use for...of (IE not supported as of time of writing)
       timeStamps.push(arr[val].earned_date);
     }
-    return {timeStamps: timeStamps};
+    return timeStamps;
   };
 
   var getDate = function(arr) {
@@ -13,7 +13,21 @@ var Data = (function() {
     for (var times in arr) {
       dates.push(arr[times].slice(0,10));
     }
-    return {dates: dates};
+    return dates;
+  };
+
+  var dateMs = function(arr) {
+    var dateString = [];
+    for (var date in arr) {
+      dateString.push(Date.parse(arr[date]));
+    }
+    return dateString;
+  };
+
+  var badgesPerDay = function(arr) {
+    //StackOverflow answer by radicand: http://stackoverflow.com/questions/5667888/counting-the-occurrences-of-javascript-array-elements
+    var obj = arr.reduce(function(countMap, word) {countMap[word] = ++countMap[word] || 1; return countMap}, {});
+    return obj;
   };
 
   var objCopy = function(obj) {
@@ -35,6 +49,15 @@ var Data = (function() {
      return dataRaw;
   };
 
+  var objToArr = function(obj) {
+    var newArray = [];
+    for (var key in obj) {
+      newArray.push([key, obj[key]]);
+    }
+    console.log(newArray);
+    return newArray;
+  };
+
   var sortPoints = function(dataArr) {
     //sort dataRaw ascending using Bostock's descending function and store as data to pass to d3.js
    var newArr = [];
@@ -53,6 +76,9 @@ var Data = (function() {
   return {
     getTimeStamps: getTimeStamps,
     getDate: getDate,
+    dateMs: dateMs,
+    badgesPerDay: badgesPerDay,
+    objToArr: objToArr,
     objCopy: objCopy,
     extractPoints: extractPoints,
     sortPoints: sortPoints
